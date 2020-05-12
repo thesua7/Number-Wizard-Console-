765c630e9740e6c80bbc0829a8842caded726601
Sani Ahamed <thesua711@gmail.com>
2020-05-13 05:27:04 +0600

    C# Basics #1

---
+++ Number Wizzard Console/Assets/NumberWizard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+       int max = 1000;
+       int min = 1;
+
+       Debug.Log("Welcome to the GAME!!"+"\n"); 
+       Debug.Log("Pick a number: "+"\n");
+       Debug.Log("Highest number is: "+max+"\n");
+       Debug.Log("Lowest number is: "+min+"\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
