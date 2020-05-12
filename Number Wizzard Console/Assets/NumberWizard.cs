a18787f3f371c9d6a41e0a1d8957b77416a9c50e
Sani Ahamed <thesua711@gmail.com>
2020-05-13 05:42:19 +0600

    Responding Key Inputs

--- Number Wizzard Console/Assets/NumberWizard.cs
+++ Number Wizzard Console/Assets/NumberWizard.cs
@@ -15,11 +15,29 @@ public class NumberWizard : MonoBehaviour
        Debug.Log("Pick a number: "+"\n");
        Debug.Log("Highest number is: "+max+"\n");
        Debug.Log("Lowest number is: "+min+"\n");
+       Debug.Log("Tell me your number is higer or lower than 500 "+"\n");
+       Debug.Log("Press UP=Higher,Press Down=Lower & Press Enter=Correct"+"\n");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+          if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up Arrow key was pressed");
+        }
+
+          if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down Arrow key was pressed");
+        }
+
+          if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Enter key was pressed");
+        }
     }
+
+
 }
